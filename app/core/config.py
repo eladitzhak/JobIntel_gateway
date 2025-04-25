c2741bc2dfb0db94291806123624c7e163ef2be0
elad itzhak <37639908+eladitzhak@users.noreply.github.com>
2025-04-25 17:05:03 +0300

    Google auth WIP

--- app/core/config.py
+++ app/core/config.py
@@ -3,14 +3,22 @@
 from pydantic_settings import BaseSettings
 from pydantic import Field
 
+
 class Settings(BaseSettings):
     debug: bool = Field(..., alias="DEBUG")
     database_url: str = Field(..., alias="DATABASE_URL")
     jwt_secret_key: str = Field(..., alias="JWT_SECRET_KEY")
     algorithm: str = Field(default="HS256", alias="ALGORITHM")
 
+    # GOOGLE AUTH
+    google_client_id: str = Field(..., alias="GOOGLE_CLIENT_ID")
+    google_client_secret: str = Field(..., alias="GOOGLE_CLIENT_SECRET")
+    google_redirect_uri: str = Field(..., alias="GOOGLE_REDIRECT_URI")
+    secret_key_for_session: str = Field(..., alias="SECRET_KEY_FOR_SESSION")
+
     class Config:
         env_file = ".env"
         extra = "ignore"
+
 
 settings = Settings()
--- app/main.py
+++ app/main.py
@@ -6,18 +6,20 @@ from app.core.database import get_db
 from sqlalchemy.ext.asyncio import AsyncSession
 
 
+app = FastAPI(title="JobIntel Gateway API")
 
-app = FastAPI()
 
 @app.get("/")
 def read_root():
     return {
-        "message": "JobIntel main app is up!",
-        "debug": settings.debug
+        "message": "🚀 JobIntel Gateway is live!",
+        "debug": settings.debug,
+        "environment": "local",
+        "version": "v1.0.0",
     }
+
 
 @app.get("/db-test")
 async def test_db(session: AsyncSession = Depends(get_db)):
     result = await session.execute(text("SELECT 1"))
     return {"db_connected": result.scalar() == 1}
-
